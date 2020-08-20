ed6bb95d8939e70321f425dc14cfdfb5e11e24cb
chaim chazan <chaimyisroel@gmail.com>
2020-08-20 22:44:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  const animal = 'dog'
+  const animal = 'cat'
   return animal
 }
 
